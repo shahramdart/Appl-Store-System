2ddd4e7e9919d00978c35ba988d962abaa49b6bf
Shahram Othman Awla <108738123+shahramdart@users.noreply.github.com>
2025-02-06 03:19:04 +0300

    Upload SQL DB

--- mobile-ranya.sql
+++ mobile-ranya.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 31, 2025 at 10:47 PM
+-- Generation Time: Feb 06, 2025 at 01:18 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -38,14 +38,6 @@ CREATE TABLE `archives` (
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `updatedAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `archives`
---
-
-INSERT INTO `archives` (`id`, `product_name`, `product_price`, `product_qty`, `description`, `category_id`, `brand_id`, `createdAt`, `updatedAt`) VALUES
-(122, NULL, NULL, 1, NULL, 2, 5, '2025-01-31 17:56:17', '2025-01-31 17:56:17'),
-(123, NULL, NULL, 1, NULL, 2, 5, '2025-01-31 20:56:47', '2025-01-31 20:56:47');
 
 -- --------------------------------------------------------
 
@@ -90,11 +82,27 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`id`, `category_name`, `user_id`, `createdAt`, `updatedAt`) VALUES
 (2, 'Mobile', 2, '2024-12-04 20:31:08', '2024-12-04 20:31:08'),
-(3, 'adapter', 2, '2024-12-04 20:31:21', '2024-12-04 20:31:21'),
+(3, 'Adapter', 2, '2024-12-04 20:31:21', '2025-02-05 23:25:09'),
 (4, 'cable', 2, '2024-12-04 20:31:34', '2024-12-04 20:31:34'),
-(5, 'case', 2, '2024-12-04 20:31:51', '2024-12-04 20:31:51'),
+(5, 'Case', 2, '2024-12-04 20:31:51', '2025-02-05 23:22:55'),
 (6, 'glass screen', 2, '2024-12-04 20:32:03', '2024-12-04 20:32:03'),
 (7, 'Tablet', 2, '2024-12-07 18:46:10', '2024-12-07 18:46:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `customer_phoneNo` varchar(24) NOT NULL,
+  `customer_address` varchar(128) DEFAULT NULL,
+  `invoice_id` int(11) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -105,7 +113,9 @@ INSERT INTO `category` (`id`, `category_name`, `user_id`, `createdAt`, `updatedA
 CREATE TABLE `expenses` (
   `id` int(11) NOT NULL,
   `total_purchase` decimal(10,2) DEFAULT 0.00,
-  `purchase_price` decimal(10,2) NOT NULL,
+  `total_purchase_dolar` decimal(10,2) DEFAULT NULL,
+  `purchase_price` decimal(10,2) DEFAULT NULL,
+  `purchase_price_dolar` decimal(10,2) DEFAULT NULL,
   `quantity` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
@@ -113,14 +123,6 @@ CREATE TABLE `expenses` (
   `updatedAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `expenses`
---
-
-INSERT INTO `expenses` (`id`, `total_purchase`, `purchase_price`, `quantity`, `category_id`, `product_id`, `createdAt`, `updatedAt`, `user_id`) VALUES
-(65, 12000.00, 1200.00, 10, 2, 119, '2025-01-31 15:09:15', '2025-01-31 15:09:15', 2),
-(69, 890.00, 890.00, 1, 2, 123, '2025-01-31 18:35:40', '2025-01-31 18:35:40', 2);
 
 -- --------------------------------------------------------
 
@@ -151,13 +153,6 @@ CREATE TABLE `installment_sales` (
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `installment_sales`
---
-
-INSERT INTO `installment_sales` (`id`, `product_id`, `user_id`, `product_price`, `quantity`, `total_price`, `paray_zyada`, `peshaky_payment`, `paray_mawa`, `qty_qist`, `total_qistakan`, `qisty_mawa`, `date`, `final_date`, `status`, `customer_name`, `customer_phoneNo`, `customer_address`, `createdAt`, `updatedAt`) VALUES
-(6, 119, 2, 1200.00, 1, 1300.00, 100.00, 500.00, 800.00, 100.00, 9, 9, '2025-02-01 00:00:00', '2025-11-01 00:00:00', 'هەڵپەسێردراو', 'Shahram Othman', '+9647507145127', '', '2025-01-31 19:25:51', '2025-01-31 19:25:51');
-
 -- --------------------------------------------------------
 
 --
@@ -169,27 +164,17 @@ CREATE TABLE `invoices` (
   `invoice_quantity` int(11) NOT NULL,
   `invoice_pirce` decimal(10,2) NOT NULL,
   `invoice_total_pirce` decimal(10,2) NOT NULL,
+  `invoice_total_pirce_dolar` decimal(10,2) DEFAULT NULL,
+  `invoice_pirce_dolar` decimal(10,2) NOT NULL,
   `invoice_status` enum('کاش','مانگانە') NOT NULL,
   `invoice_date` datetime NOT NULL,
   `invoice_customer` varchar(64) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `createdAt` datetime NOT NULL,
-  `updatedAt` datetime NOT NULL
+  `updatedAt` datetime NOT NULL,
+  `sale_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `invoices`
---
-
-INSERT INTO `invoices` (`id`, `invoice_quantity`, `invoice_pirce`, `invoice_total_pirce`, `invoice_status`, `invoice_date`, `invoice_customer`, `user_id`, `product_id`, `createdAt`, `updatedAt`) VALUES
-(40, 1, 1215.00, 1215.00, 'کاش', '2025-01-31 00:00:00', 'Walk-in', 2, 119, '2025-01-31 15:09:46', '2025-01-31 15:09:46'),
-(48, 1, 1220.00, 1220.00, 'کاش', '2025-01-31 00:00:00', 'Walk-in', 2, 119, '2025-01-31 17:50:23', '2025-01-31 17:50:23'),
-(50, 1, 1220.00, 1220.00, 'کاش', '2025-01-31 00:00:00', 'Walk-in', 2, 119, '2025-01-31 17:53:49', '2025-01-31 17:53:49'),
-(54, 1, 1220.00, 1220.00, 'کاش', '2025-01-31 00:00:00', 'شەهرام عثمان', 2, 119, '2025-01-31 18:21:50', '2025-01-31 18:21:50'),
-(55, 1, 1220.00, 1220.00, 'کاش', '2025-01-31 00:00:00', 'شەهرام عثمان', 2, 119, '2025-01-31 18:23:11', '2025-01-31 18:23:11'),
-(57, 1, 1220.00, 1220.00, 'کاش', '2025-01-31 00:00:00', 'Walk-in', 2, 119, '2025-01-31 18:47:30', '2025-01-31 18:47:30'),
-(58, 1, 900.00, 900.00, 'کاش', '2025-01-31 00:00:00', 'فەرەیدون', 2, 123, '2025-01-31 20:56:47', '2025-01-31 20:56:47');
 
 -- --------------------------------------------------------
 
@@ -223,6 +208,7 @@ CREATE TABLE `products` (
   `product_name` varchar(255) NOT NULL,
   `product_color` varchar(100) DEFAULT NULL,
   `product_price` decimal(10,2) DEFAULT NULL,
+  `product_price_dolar` decimal(10,2) DEFAULT NULL,
   `product_qty` int(11) NOT NULL,
   `product_qrcode` bigint(20) NOT NULL,
   `category_id` int(11) NOT NULL,
@@ -231,14 +217,6 @@ CREATE TABLE `products` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`id`, `product_name`, `product_color`, `product_price`, `product_qty`, `product_qrcode`, `category_id`, `brand_id`, `user_id`, `createdAt`, `updatedAt`) VALUES
-(119, 'iphone 16 pro max', 'Titanium', 1200.00, 4, 202405231791, 2, 5, 2, '2025-01-31 15:09:15', '2025-01-31 18:47:30'),
-(123, 'iphone 14 pro max', 'Purple ', 890.00, 0, 352680943746050, 2, 5, 2, '2025-01-31 18:35:40', '2025-01-31 20:56:47');
 
 -- --------------------------------------------------------
 
@@ -250,8 +228,10 @@ CREATE TABLE `sales` (
   `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `price` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `price` decimal(10,2) DEFAULT NULL,
   `total_price` decimal(10,2) NOT NULL,
+  `price_dolar` decimal(10,2) DEFAULT NULL,
+  `total_price_dolar` decimal(10,2) DEFAULT NULL,
   `profit_amount` decimal(10,2) NOT NULL,
   `discount` decimal(10,2) NOT NULL,
   `category_id` int(11) DEFAULT NULL,
@@ -261,19 +241,6 @@ CREATE TABLE `sales` (
   `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `sale_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `sales`
---
-
-INSERT INTO `sales` (`id`, `product_id`, `quantity`, `price`, `total_price`, `profit_amount`, `discount`, `category_id`, `brand_id`, `user_id`, `createdAt`, `updatedAt`, `sale_date`) VALUES
-(35, 119, 1, 1215.00, 1215.00, 15.00, 0.00, 2, 5, 2, '2025-01-31 15:09:46', '2025-01-31 15:09:46', '2025-01-31 00:00:00'),
-(38, 119, 1, 1220.00, 1220.00, 20.00, 0.00, 2, 5, 2, '2025-01-31 17:50:23', '2025-01-31 17:50:23', '2025-01-31 00:00:00'),
-(40, 119, 1, 1220.00, 1220.00, 20.00, 0.00, 2, 5, 2, '2025-01-31 17:53:49', '2025-01-31 17:53:49', '2025-01-31 00:00:00'),
-(42, 119, 1, 1220.00, 1220.00, 20.00, 0.00, 2, 5, 2, '2025-01-31 18:21:50', '2025-01-31 18:21:50', '2025-01-31 00:00:00'),
-(43, 119, 1, 1220.00, 1220.00, 20.00, 0.00, 2, 5, 2, '2025-01-31 18:23:11', '2025-01-31 18:23:11', '2025-01-31 00:00:00'),
-(44, 119, 1, 1220.00, 1220.00, 20.00, 0.00, 2, 5, 2, '2025-01-31 18:47:30', '2025-01-31 18:47:30', '2025-01-31 00:00:00'),
-(45, 123, 1, 900.00, 900.00, 10.00, 0.00, 2, 5, 2, '2025-01-31 20:56:47', '2025-01-31 20:56:47', '2025-01-31 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -286,6 +253,8 @@ CREATE TABLE `sallings` (
   `salling_date` datetime NOT NULL,
   `salling_quantity` int(11) DEFAULT NULL,
   `salling_price` float DEFAULT NULL,
+  `salling_price_dolar` decimal(10,2) NOT NULL,
+  `salling_total_price_dolar` decimal(10,2) DEFAULT NULL,
   `salling_discount` float NOT NULL,
   `price_after_discount` decimal(10,2) NOT NULL,
   `salling_total_price` decimal(10,2) NOT NULL,
@@ -298,19 +267,6 @@ CREATE TABLE `sallings` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `sallings`
---
-
-INSERT INTO `sallings` (`id`, `salling_date`, `salling_quantity`, `salling_price`, `salling_discount`, `price_after_discount`, `salling_total_price`, `salling_description`, `salling_status`, `product_id`, `category_id`, `brand_id`, `user_id`, `createdAt`, `updatedAt`) VALUES
-(37, '2025-01-31 00:00:00', 1, 1215, 0, 1215.00, 1215.00, '', 'کاش', 119, 2, 5, 2, '2025-01-31 15:09:46', '2025-01-31 15:09:46'),
-(45, '2025-01-31 00:00:00', 1, 1220, 0, 1220.00, 1220.00, '', 'کاش', 119, 2, 5, 2, '2025-01-31 17:50:23', '2025-01-31 17:50:23'),
-(47, '2025-01-31 00:00:00', 1, 1220, 0, 1220.00, 1220.00, '', 'کاش', 119, 2, 5, 2, '2025-01-31 17:53:49', '2025-01-31 17:53:49'),
-(51, '2025-01-31 00:00:00', 1, 1220, 0, 1220.00, 1220.00, '', 'کاش', 119, 2, 5, 2, '2025-01-31 18:21:50', '2025-01-31 18:21:50'),
-(52, '2025-01-31 00:00:00', 1, 1220, 0, 1220.00, 1220.00, '', 'کاش', 119, 2, 5, 2, '2025-01-31 18:23:11', '2025-01-31 18:23:11'),
-(54, '2025-01-31 00:00:00', 1, 1220, 0, 1220.00, 1220.00, '', 'کاش', 119, 2, 5, 2, '2025-01-31 18:47:30', '2025-01-31 18:47:30'),
-(55, '2025-01-31 00:00:00', 1, 900, 0, 900.00, 900.00, '', 'کاش', 123, 2, 5, 2, '2025-01-31 20:56:47', '2025-01-31 20:56:47');
 
 -- --------------------------------------------------------
 
@@ -331,8 +287,12 @@ CREATE TABLE `Sessions` (
 --
 
 INSERT INTO `Sessions` (`sid`, `expires`, `data`, `createdAt`, `updatedAt`) VALUES
-('8-ttFvCazqFDs7qbuQZENm58n56NR_Pd', '2025-02-01 21:47:53', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"userId\":2}', '2025-01-31 19:17:59', '2025-01-31 21:47:53'),
-('T29F-3RzwTSF1nz2Ugmz8MyFOYUHaHh0', '2025-02-01 21:07:40', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"userId\":2}', '2025-01-31 19:21:13', '2025-01-31 21:07:40');
+('9Uwj4vFjV7oZzbL3Q_3H9q2n_doDheYB', '2025-02-06 23:55:59', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"userId\":2}', '2025-02-03 09:06:11', '2025-02-05 23:55:59'),
+('EQG_H-DyStzXr6UCxteQfJy4MgnXuWXG', '2025-02-06 17:16:58', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"}}', '2025-02-05 17:16:58', '2025-02-05 17:16:58'),
+('mIMrQoYz2l8kn9klngia4xJEpInwxoou', '2025-02-06 17:16:58', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"}}', '2025-02-05 17:16:58', '2025-02-05 17:16:58'),
+('v0Nnlk9Rf6VJKriqUCZfCeCheqpcLNOW', '2025-02-07 00:15:33', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"userId\":2}', '2025-02-06 00:07:03', '2025-02-06 00:15:33'),
+('x0BHV7ELo9C2zMl479QyzHELSaCjFIhz', '2025-02-06 12:22:49', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"userId\":2}', '2025-02-03 20:05:36', '2025-02-05 12:22:49'),
+('zjJ3eTjL78KBE5HbVA61WdpMLi368bDH', '2025-02-06 17:16:58', '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"secure\":false,\"httpOnly\":true,\"path\":\"/\"}}', '2025-02-05 17:16:58', '2025-02-05 17:16:58');
 
 -- --------------------------------------------------------
 
@@ -356,9 +316,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_phone`, `permission_id`, `createdAt`, `updatedAt`) VALUES
-(2, 'Sinan Aras Yilmaz', 'sinan@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$kFgMRbfU8kBZJKERFMYq4Q$REKXvRDv+a8PIZlJHPxDcUvkiiBMW5kuyASMknNSxGY', '12343215432123', 1, '2024-11-09 15:25:44', '2024-11-09 15:25:44'),
-(8, 'Bavel Majid', 'bavel@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$yiRkUgaKgS/7ilrsfUeL3Q$b24+dxcAfNwKFclEMxetlr7pl5HH9jYL633KasiKra8', '11223344', 2, '2024-12-17 11:51:41', '2024-12-17 11:51:41'),
-(9, 'فەرەیدون محمد', 'faraidun@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$o7wRG6hQzu+Pd1G6G3LNOQ$YM7+VlRB9iKCmI7c9HVv+xzkc+C5bTpn4/9N9gpKNp4', '07707704648', 1, '2025-01-30 17:19:58', '2025-01-30 17:19:58');
+(2, 'Sinan Aras Yilmaz', 'sinan@gmail.com', '$argon2id$v=19$m=65536,t=3,p=4$kFgMRbfU8kBZJKERFMYq4Q$REKXvRDv+a8PIZlJHPxDcUvkiiBMW5kuyASMknNSxGY', '12343215432123', 1, '2024-11-09 15:25:44', '2024-11-09 15:25:44');
 
 --
 -- Indexes for dumped tables
@@ -397,6 +355,13 @@ ALTER TABLE `category`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customers_ibfk_1` (`invoice_id`);
+
+--
 -- Indexes for table `expenses`
 --
 ALTER TABLE `expenses`
@@ -419,7 +384,8 @@ ALTER TABLE `installment_sales`
 ALTER TABLE `invoices`
   ADD PRIMARY KEY (`id`),
   ADD KEY `invoices_ibfk_1` (`user_id`),
-  ADD KEY `invoices_ibfk_2` (`product_id`);
+  ADD KEY `invoices_ibfk_2` (`product_id`),
+  ADD KEY `fk_sale_id` (`sale_id`);
 
 --
 -- Indexes for table `permission`
@@ -483,19 +449,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `expenses`
 --
 ALTER TABLE `expenses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT for table `installment_sales`
@@ -507,7 +479,7 @@ ALTER TABLE `installment_sales`
 -- AUTO_INCREMENT for table `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 
 --
 -- AUTO_INCREMENT for table `permission`
@@ -519,19 +491,19 @@ ALTER TABLE `permission`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
 
 --
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 
 --
 -- AUTO_INCREMENT for table `sallings`
 --
 ALTER TABLE `sallings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -563,6 +535,12 @@ ALTER TABLE `category`
   ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
+-- Constraints for table `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `invoices` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `expenses`
 --
 ALTER TABLE `expenses`
@@ -580,6 +558,7 @@ ALTER TABLE `installment_sales`
 -- Constraints for table `invoices`
 --
 ALTER TABLE `invoices`
+  ADD CONSTRAINT `fk_sale_id` FOREIGN KEY (`sale_id`) REFERENCES `sales` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `invoices_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `invoices_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
